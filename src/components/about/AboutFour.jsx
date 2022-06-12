22d821d0646d5b574198521be831b2a694fe1d95
Kristen Reid <kreiddetroit@gmail.com>
2022-06-12 01:26:44 -0400

    revisions to about me description

--- src/components/about/AboutFour.jsx
+++ src/components/about/AboutFour.jsx
@@ -33,10 +33,13 @@ const AboutFour = () => {
                   <p>
                     Full Stack Software Engineer with experience in building full stack web applications using languages,
                     frameworks, and libraries including React, Python, JavaScript, Flask, Express, PostgreSQL.
-                    I've been one throughout the years to wear many hats. While having many job titles and working in different environments
+                    I've been one throughout the years to wear many hats (customer service, hospitality and other job titles). While having many job titles and working in different environments
                     I have acquired many valuable skills including 20+ years of customer service, being a part of different teams learning how to work in group environments well,
                     and being able to adapt quickly to any environment I was placed in.
-                    I left the path to wearing many hats to pursue a career in an industry I have loved and admired for many years.
+                    I left that path to pursue a career in an industry I have loved and admired for many years.
+                    After attending App Academy for 1000+ hours of course work (not including studying and working on projects) for 24 weeks in their online/remote program where I learned many
+                    languages and frameworks such as Python, JavaScript, React, Express, and Postgresql. 2 of those projects I worked on with a team of Software Engineers
+                    where we collaborated in building applications together, and learned how to effectively work as a team remotely to successfully build dynamic smooth functioning web applications.
                   </p>
                 </div>
                 <div className="shane_tm_button">
